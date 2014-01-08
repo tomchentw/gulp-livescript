ee0322a5b68792170ee764fb585f2e5c845d4be2
tomchentw <developer@tomchentw.com>
2014-01-08 22:11:43 +0800

    test(mocha): add bare test case

--- src/main.ls
+++ src/main.ls
@@ -24,3 +24,20 @@ it 'should compile livescript file to javascript' !(done) ->
     done!
 
   ls.write fakeFile
+
+it 'should compile livescript with bare option to javascript' !(done) ->
+  const ls = gulp-livescript bare: true
+  const fakeFile = new gutil.File do
+      base: 'test/fixtures'
+      cwd: 'test/fixtures'
+      path: 'test/fixtures/file.ls'
+      contents: fs.readFileSync 'test/fixtures/file.ls'
+
+  ls.once 'data' !(expectedFile) ->
+    should.exist expectedFile
+    should.exist expectedFile.path
+    should.exist expectedFile.contents 
+    String expectedFile.contents .should.equal fs.readFileSync('test/fixtures/bare-expected.js', 'utf8')
+    done!
+
+  ls.write fakeFile
--- test/main.js
+++ test/main.js
@@ -24,5 +24,25 @@
       });
       ls.write(fakeFile);
     });
+    it('should compile livescript with bare option to javascript', function(done){
+      var ls, fakeFile;
+      ls = gulpLivescript({
+        bare: true
+      });
+      fakeFile = new gutil.File({
+        base: 'test/fixtures',
+        cwd: 'test/fixtures',
+        path: 'test/fixtures/file.ls',
+        contents: fs.readFileSync('test/fixtures/file.ls')
+      });
+      ls.once('data', function(expectedFile){
+        should.exist(expectedFile);
+        should.exist(expectedFile.path);
+        should.exist(expectedFile.contents);
+        String(expectedFile.contents).should.equal(fs.readFileSync('test/fixtures/bare-expected.js', 'utf8'));
+        done();
+      });
+      ls.write(fakeFile);
+    });
   });
 }).call(this);
