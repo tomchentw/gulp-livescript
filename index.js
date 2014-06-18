8f2e3322d8a6a67f34d2b9276d5fd0befb96157c
Tom Chen <developer@tomchentw.com>
2014-06-18 10:03:02 +0800

    chore(release): e96b9870da by Makefile

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-var through2, LiveScript, gutil, PluginError;
+var through2, LiveScript, gutil;
 through2 = require('through2');
 LiveScript = require('LiveScript');
 gutil = require('gulp-util');
-PluginError = gutil.PluginError;
 module.exports = function(options){
   options || (options = {});
   function modifyLS(file, enc, done){
-    var ref$, e, this$ = this;
-    function emitError(it){
-      this$.emit('error', new PluginError('gulp-livescript', it));
-      done();
-    }
+    var error, ref$;
     if (file.isNull()) {
-      return done();
+      return done(null, file);
     }
     if (file.isStream()) {
-      return emitError('Streaming not supported');
+      error = 'Streaming not supported';
+    } else {
+      try {
+        file.contents = new Buffer(LiveScript.compile(file.contents.toString('utf8'), (ref$ = {}, import$(ref$, options), ref$.filename = file.path, ref$)));
+        file.path = gutil.replaceExtension(file.path, '.js');
+      } catch (e$) {
+        error = e$;
+      }
     }
-    try {
-      file.contents = new Buffer(LiveScript.compile(file.contents.toString('utf8'), (ref$ = {}, import$(ref$, options), ref$.filename = file.path, ref$)));
-      file.path = gutil.replaceExtension(file.path, '.js');
-    } catch (e$) {
-      e = e$;
-      return emitError(e);
+    if (error) {
+      error = new gutil.PluginError('gulp-livescript', error);
     }
-    this.push(file);
-    done();
+    done(error, file);
   }
   return through2.obj(modifyLS);
 };
